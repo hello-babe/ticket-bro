c5d12df7ea31a8a7b919f6a40ec79522c4211f2e
sihab-hasan <sihabsiuuu@gmail.com>
2026-03-08 23:08:13 +0600

    remove unnecessary files and folders

--- frontend/tests/mocks/data/users.js
+++
@@ -1,7 +0,0 @@
-import { createMockUser } from '../generator';
-
-export const mockUsers = [
-  createMockUser('ADMIN'),
-  createMockUser('ORGANIZER'),
-  ...Array.from({ length: 5 }, () => createMockUser('USER')),
-];
