fbde9ddf74952e3d262ccc5fbba6e5706017fde7
sihab-hasan <sihabsiuuu@gmail.com>
2026-03-11 17:09:48 +0600

    feat(refactor): reorganize components folder structure

--- frontend/src/components/shared/index.js
+++
@@ -1,7 +0,0 @@
-// frontend/src/components/shared/index.js
-
-export { default as Breadcrumb } from "./Breadcrumb";
-export { default as InputField } from "./InputField";
-export { default as Button } from "./Button";
-export { default as Divider } from "./Divider";
-export { default as UnverifiedBanner } from "./UnverifiedBanner";
